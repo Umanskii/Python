0a2e5defcfdad0f376937196c425c05382dde4d8
umanskiisd <devopsaiproject@gmail.com>
2024-02-04 23:37:51 -0600

    Updated 1M house project

---
+++ If Staitment.py
@@ -0,0 +1,15 @@
+#swithing case between True and False
+is_hot = False
+is_cold = True
+
+if is_hot: 
+    print("It is a hot day") 
+    print("Drink plenty of water") #will run if Is_hot is True
+elif is_cold:
+    print("It is a cold day")
+    print("Wear warm clothes")#will run if Is_cold is True
+else:
+    print("Its a lovely day ")  #will run if Is_hot is False and Is_cold is False
+print('Enjoy your day') #will allway show this massage, this print out of the if statement
+
+
