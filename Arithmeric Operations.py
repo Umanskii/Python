2fd3a8167dbffe03fd32a952c0b0cad5b8ee54be
umanskiisd <devopsaiproject@gmail.com>
2024-02-04 22:18:11 -0600

    add more python filles

---
+++ Arithmeric Operations.py
@@ -0,0 +1,15 @@
+# +
+#Arithmetic Operators
+print (10/3) #float division 
+print (10//3) #integer division 
+print (10%3) #modulo division (remender of devision)    
+print (10*3) #multiplication 
+print (10**2) #power of 2
+
+
+x = 10
+x = x + 3
+x += 3  #augmented assignment operator same as line 11 or x -= 3 
+print (x)
+#Example of python output 13
+
---
+++ Operator Precedence.py
@@ -0,0 +1,12 @@
+x = 10 + 3 * 2 ** 2
+print (x)
+
+#Function output  = 22 means python will fallow the math order of operations 
+#Exponention 2 ** 3 
+#Multiplication or division 
+#Addition or subtraction 
+
+#parentheses will change the order of operations 
+
+x2 = (10 + 3) * 2 ** 2
+print (x2) #print 52
