c1cce1c40a99e89eafba58b77fae316dba02d0ef
umanskiisd <devopsaiproject@gmail.com>
2024-02-04 23:55:55 -0600

    Updated 1M house project

---
+++ Downpayment calculator.py
@@ -0,0 +1,13 @@
+#still working on it
+price = input("Enter the House price: ")
+good_credit = input("Enter credit score: ")
+if good_credit  >= 700:True
+else bed_credit >= 500:False
+print(good_credit)
+
+if good_credit:
+    downpayment = price * 0.1
+    print(f"Down payment is ${downpayment}")
+else:
+    downpayment = price * 0.2
+    print(f"Down payment is ${downpayment}")
