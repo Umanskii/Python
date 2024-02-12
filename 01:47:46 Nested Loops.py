16c4ac684be73d6f84a28dcf079e4bee91e38987
umanskiisd <devopsaiproject@gmail.com>
2024-02-11 19:13:43 -0600

    challage done

---
+++ 01:47:46 Nested Loops.py
@@ -0,0 +1,5 @@
+# Nested loops  is loop in loop. look for nested loops example bellow
+
+for x in range(4):
+    for y in range(3):
+        print (f'({x}, {y})')
---
+++ 1:51:19 Challange.py
@@ -0,0 +1,12 @@
+# Print letter F 
+
+numbers = [5,2,5,2,2]
+for number in numbers:
+    print (number * 'x')
+
+
+# Print letter L
+    
+numbers = [2,2,2,6,6]
+for number in numbers:
+    print (number * 'x')
